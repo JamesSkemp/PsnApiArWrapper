71b713f1091b9b89d820e7859e2c06e5111b3292
James Skemp <strivinglife@gmail.com>
2013-06-23 14:01:52 -0500

    Added functionality to pull game data.
    Can now get trophies for an individual game.

--- PsnApiArWrapper/Games.cs
+++ PsnApiArWrapper/Games.cs
@@ -3,6 +3,19 @@
 	public partial class PsnApi
 	{
 		/// <summary>
+		/// Get an individual game for an individual user.
+		/// <seealso cref="GetTrophies"/>
+		/// </summary>
+		/// <param name="psnId">Id of the user to query.</param>
+		/// <param name="gameId">Id of the game to query.</param>
+		/// <returns>String of data for the user and game.</returns>
+		public string GetGame(string psnId, string gameId)
+		{
+			var parameters = string.Format("sPSNID={0}&sIdGame={1}", psnId, gameId);
+			return ApiRequest("getGame", parameters);
+		}
+
+		/// <summary>
 		/// Get games for an individual user.
 		/// </summary>
 		/// <param name="psnId">Id of the user to query.</param>
---
+++ PsnApiArWrapper/Trophies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsnApiArWrapper
+{
+	public partial class PsnApi
+	{
+		/// <summary>
+		/// Get trophies for a particular game for an individual user.
+		/// <seealso cref="GetGame"/>
+		/// </summary>
+		/// <param name="psnId">Id of the user to query.</param>
+		/// <param name="gameId">Id of the game to get trophies for.</param>
+		/// <returns>String of data for the user.</returns>
+		public string GetTrophies(string psnId, string gameId)
+		{
+			var parameters = string.Format("sPSNID={0}&sGameId={1}", psnId, gameId);
+			return ApiRequest("getTrophies", parameters);
+		}
+	}
+}
